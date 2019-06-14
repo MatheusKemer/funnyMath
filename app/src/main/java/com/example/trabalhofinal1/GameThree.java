b0d7b84f2813eb3c0c3c9f7de1a190befb16a521
Matheus <97kemer@gmail.com>
2019-06-14 15:24:08 -0300

    some more changes

--- app/src/main/java/com/example/trabalhofinal1/GameThree.java
+++ app/src/main/java/com/example/trabalhofinal1/GameThree.java
@@ -13,7 +13,7 @@ import java.util.Arrays;
 import java.util.Collections;
 
 class GameThree extends AppCompatActivity {
-    public ArrayList<Integer> numbers = new ArrayList<Integer>(Arrays.asList(1,2,3,4,5,6,7,8,9,10));
+    public ArrayList<Integer> numbers = new ArrayList<Integer>(Arrays.asList(0,1,2,3,4,5,6,7,8,9));
     private int currentCorrectAnswer;
     private int generalScore = 0;
     public int round = 1;
